5b162fb51e44d6e25fa958fd49f9cdb107158c65
Jorge Luis Prieto Diaz <pdjorgeluis@gmail.com>
2024-06-06 12:08:03 -0400

    organized index.js

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-require('dotenv').config()
 const express = require( 'express' ) 
 const app = express()
-const morgan  = require('morgan');
+require('dotenv').config()
+
+//const morgan  = require('morgan');
 
 const Person = require('./models/person')
-const mongoose = require('mongoose')
+//app.use(express.static('build'))
 
-app.use(express.json());
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
 
-const cors = require('cors');
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+const cors = require('cors'); 
 //const { default: mongoose } = require('mongoose');
-app.use(cors())
 
+app.use(cors())
+app.use(express.json());
+app.use(requestLogger)
+
+
+
+/*
 morgan.token('host', (request, response) => {
     return request.hostname;
 });
-
-
+*/
+/*
 let persons = [
     { 
       "id": 1,
@@ -38,9 +61,14 @@ let persons = [
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
     }
-]
+]*/
 
-app.use(morgan(':method :host :status :res[content-length] - :response-time ms :body')); //app.use(morgan('tiny'));
+//app.use(morgan(':method :host :status :res[content-length] - :response-time ms :body')); //app.use(morgan('tiny'));
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
 //Home
 app.get('/', (request, response) => {
     const fURL = request.protocol + '://' + request.get('host');
@@ -85,21 +113,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-// DELETE a person
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
-    
-    if(person){
-        persons = persons.filter(person => person.id !== id);
-        response.status(204).end();
-    }else{
-        response.statusMessage = 'The Note was not found';
-        response.status(404).end();
-    }
-    
-})
-
+/*
+morgan.token('body', (request, response) => {
+    return JSON.stringify(request.body);
+});
+*/
+//ADD (POST) a person
 //id generator
 const generateId = () => {
     const maxId = persons.length > 0
@@ -108,11 +127,6 @@ const generateId = () => {
     return maxId + 1;
 }
 
-morgan.token('body', (request, response) => {
-    return JSON.stringify(request.body);
-});
-
-//ADD (POST) a person
 app.post('/api/persons', (request, response) => {
     const body = request.body;
     
@@ -146,9 +160,24 @@ app.post('/api/persons', (request, response) => {
     //response.json(person);
 })
 
-const unknownEndPoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
-}
+// DELETE a person
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(person => person.id === id);
+    
+    if(person){
+        persons = persons.filter(person => person.id !== id);
+        response.status(204).end();
+    }else{
+        response.statusMessage = 'The Note was not found';
+        response.status(404).end();
+    }
+    
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
 const port = process.env.PORT || 3001 //setting port and environment variable
 app.listen(port, () => {
     console.log(`Server running on PORT ${port}`);
